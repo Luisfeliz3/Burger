146d6fe6ae3a3d0ece4bef5d9d24d91a6789b2ef
Luis Feliz <Luisfeliz@gmail.com>
2020-11-06 18:19:12 -0500

    chore(Burger): adding heroku profile

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger (burger_name, devour) values ('Bacon',1);
+INSERT INTO burger (burger_name, devour) values ('Cheese',0);
+INSERT INTO burger (burger_name, devour) values ('PBE',1);
+INSERT INTO burger (burger_name, devour) values ('Veggie',0);
+INSERT INTO burger (burger_name, devour) values ('Lamb',1);
+INSERT INTO burger (burger_name, devour) values ('Beef',0);
+INSERT INTO burger (burger_name, devour) values ('Whopper',1);
+INSERT INTO burger (burger_name, devour) values ('Double',0);
