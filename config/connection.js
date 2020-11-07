daa75e12a0ec59e2477c352713bbdfe7fa92cbd0
Luis Feliz <Luisfeliz@gmail.com>
2020-11-06 21:01:07 -0500

    chore(Blah): adding files and logic

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,24 @@ var connection = mysql.createConnection({
 });
 
 
+if (process.env.JAWSDB_URL){
+  connection= mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database: "burgers_db"
+
+  })
+}
+
+
+
+
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
