974c68abed23c5855ca2f3ee352a032602bf634f
Luis Feliz <Luisfeliz@gmail.com>
2020-11-06 20:44:01 -0500

    chore(Blah): adding files and logic

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,5 @@
 var mysql = require("mysql");
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
 
-  }
-  else {
-    connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '12345678',
-  database: 'burgers_db'
-    }
-    )}
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -19,6 +8,7 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -26,7 +16,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
-
 
 module.exports = connection;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,3 @@
-INSERT INTO burger (burger_name, devour) values ('Bacon',1);
-INSERT INTO burger (burger_name, devour) values ('Cheese',0);
-INSERT INTO burger (burger_name, devour) values ('PBE',1);
-INSERT INTO burger (burger_name, devour) values ('Veggie',0);
-INSERT INTO burger (burger_name, devour) values ('Lamb',1);
-INSERT INTO burger (burger_name, devour) values ('Beef',0);
-INSERT INTO burger (burger_name, devour) values ('Whopper',1);
-INSERT INTO burger (burger_name, devour) values ('Double',0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
